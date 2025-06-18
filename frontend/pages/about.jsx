3401d6319a9f9b7d10eac5937f31132ed5d1c3a7
Divyansh Jindal <divyanshjindal95.2012@gmail.com>
2025-06-19 00:40:47 +0530

    Chanding name of technical secy

--- frontend/pages/about.jsx
+++ frontend/pages/about.jsx
@@ -2,21 +2,42 @@ import React from "react";
 
 const About = () => {
   return (
-    <div style={{ padding: "20px", maxWidth: "800px", margin: "0 auto", textAlign: "center" }}>
+    <div
+      style={{
+        padding: "20px",
+        maxWidth: "800px",
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
       <h1>About Us</h1>
-      
+
       <div style={{ textAlign: "left", marginTop: "20px" }}>
         <h2>Developer</h2>
-        <p><strong>Divyansh Jindal</strong></p>
-        <p>Email: <a href="mailto:b24121@students.iitmandi.ac.in">b24121@students.iitmandi.ac.in</a></p>
+        <p>
+          <strong>Divyansh Jindal</strong>
+        </p>
+        <p>
+          Email:{" "}
+          <a href="mailto:b24121@students.iitmandi.ac.in">
+            b24121@students.iitmandi.ac.in
+          </a>
+        </p>
         <p>Phone: +91 76260 40100</p>
       </div>
 
       <div style={{ textAlign: "left", marginTop: "20px" }}>
         <h2>Technical Secretary</h2>
-        <p><strong>Vaibhav Kesharwani</strong></p>
-        <p>Email: <a href="mailto:technical_secretary@students.iitmandi.ac.in">technical_secretary@students.iitmandi.ac.in</a></p>
-        <p>Phone: +91 93690 80567</p>
+        <p>
+          <strong>Aryan Singh</strong>
+        </p>
+        <p>
+          Email:{" "}
+          <a href="mailto:technical_secretary@students.iitmandi.ac.in">
+            technical_secretary@students.iitmandi.ac.in
+          </a>
+        </p>
+        <p>Phone: +91 63930 51440</p>
       </div>
 
       <div style={{ marginTop: "30px" }}>
@@ -34,15 +55,23 @@ const About = () => {
       </div>
 
       <div style={{ marginTop: "20px", fontSize: "1em", opacity: 0.9 }}>
-        <h2 style={{ fontSize: "1.1em", fontWeight: "500", marginBottom: "5px", }}>
+        <h2
+          style={{ fontSize: "1.1em", fontWeight: "500", marginBottom: "5px" }}
+        >
           Contributors
         </h2>
         <p style={{ margin: "0", fontSize: "0.85em", lineHeight: "1.4" }}>
-          <span style={{ fontWeight: "600" }}>Dhairya Sharma</span> - 
-          <a href="mailto:b24241@students.iitmandi.ac.in"> b24241@students.iitmandi.ac.in</a>
+          <span style={{ fontWeight: "600" }}>Dhairya Sharma</span> -
+          <a href="mailto:b24241@students.iitmandi.ac.in">
+            {" "}
+            b24241@students.iitmandi.ac.in
+          </a>
           <br />
-          <span style={{ fontWeight: "600" }}>Ojasvi Jain</span> - 
-          <a href="mailto:b24208@students.iitmandi.ac.in"> b24208@students.iitmandi.ac.in</a>
+          <span style={{ fontWeight: "600" }}>Ojasvi Jain</span> -
+          <a href="mailto:b24208@students.iitmandi.ac.in">
+            {" "}
+            b24208@students.iitmandi.ac.in
+          </a>
         </p>
       </div>
     </div>
